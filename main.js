94ab14ae68d1500bb3143585b03728f11838bb61
EC2 Default User <ec2-user@ip-172-31-16-83.ec2.internal>
2026-02-16 11:42:59 +0000

    Added dynamic load and ramp strategy

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-export const options = __ENV.LOAD_STATEGY === 'ramp'
-?{
-stages: [
+export const options = __ENV.LOAD_STRATEGY === 'ramp'
+  ? {
+      stages: [
         { duration: '30s', target: 5 },
         { duration: '30s', target: 15 },
         { duration: '30s', target: 0 },
       ],
     }
-: {
+  : {
       vus: __ENV.VUS ? parseInt(__ENV.VUS) : 10,
       duration: __ENV.DURATION || '1m',
     };
-   
 
 export default function () {
   const res = http.get('https://test.k6.io');
